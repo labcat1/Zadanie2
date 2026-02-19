490fe0283be661351925b05fb96243173d88389b
labcat_ <boldyrev0205@gmail.com>
2026-02-19 11:47:24 +0700

    Updated main
    Added function

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <math.h>
+bool isPrime(int possible)
+{
+    int tester = 2;
+    while (tester < (sqrt(possible) + 1)) {
+        if ((possible % tester) == 0 && (possible != tester)) {
+            return false;
+        }
+        tester++;
+    }
+    return true;
+}
 int main(void)
 {
-    int tester;
-    int possible = 1;
+    int possible = 2;
     float input;
-    int is_prime;
     scanf("%f", &input);
     while (possible < input) {
-        tester = 2;
-        while (tester<(sqrt(possible)+1) && is_prime != 0) {
-            if (((possible % tester) == 0) && (possible != tester)) {
-                is_prime = 0;
-                break;
-            }
-            tester++;
+        if (isPrime(possible)) {
+            printf("%d\n", possible);
         }
-        if (is_prime == 1) {
-            printf("%d \n",(int)possible);
-        }
-        is_prime = 1;
         possible++;
     }
 }
