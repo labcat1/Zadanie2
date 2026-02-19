66ea98e4a3fe07ce81d8cfb9cc136935e84747fb
labcat_ <boldyrev0205@gmail.com>
2026-02-19 11:24:28 +0700

    Changed main

--- main.c
+++ main.c
@@ -2,25 +2,24 @@
 #include <math.h>
 int main(void)
 {
-    float tester;
-    float possible = 1;
-    float N;
-    float k;
-    int c;
-    scanf("%f", &N);
-    while (possible<N) {
+    int tester;
+    int possible = 1;
+    float input;
+    int is_prime;
+    scanf("%f", &input);
+    while (possible < input) {
         tester = 2;
-        while (tester<(sqrt(possible)+1) && c != 1) {
-            k = possible/tester;
-            if ((k == (int)k) && (possible != tester)) {
-            c = 1;
+        while (tester<(sqrt(possible)+1) && is_prime != 0) {
+            if (((possible % tester) == 0) && (possible != tester)) {
+                is_prime = 0;
+                break;
             }
             tester++;
         }
-        if (c == 0) {
+        if (is_prime == 1) {
             printf("%d \n",(int)possible);
         }
-        c = 0;
+        is_prime = 1;
         possible++;
     }
 }
