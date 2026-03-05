4cde2d94d304650e657ac41d8e6cd27a1732fa86
labcat_ <boldyrev0205@gmail.com>
2026-03-05 11:40:15 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
 #include <math.h>
-double piPart(double number) {
-    double part = 4/(2*number-1);
-    if ((int)number%2 == 0) {
-        part = -1*part;
-    }
-    return part;
-}
-double pi(double limit) {
-    double pi1 = piPart(1);
-    double pi2 = pi1 + piPart(2);
-    double pi3 = pi2 + piPart(3);
-    double n = 4;
-    while (true) {
-        if (fabs((pi1 + pi2)/2 - (pi2 + pi3)/2) < limit){return (int)((pi3 + pi2)/(2*limit*10));}
-        else {
-            pi1 = pi2; pi2 = pi3; pi3 = pi3 + piPart(n);
-            n++;
+bool isPrime(int possible)
+{
+    int tester = 2;
+    while (tester < (sqrt(possible) + 1)) {
+        if ((possible % tester) == 0 && (possible != tester)) {
+            return false;
         }
+        tester++;
     }
+    return true;
 }
 int main(void)
 {
-    double input;
-    scanf("%lf", &input);
-    double limit = 1/pow(10, (input+1));
-    double the_pi = pi(limit);
-    printf("%.*lf\n",(int)input, (double)the_pi*limit*10);
+    int possible = 2;
+    float input;
+    scanf("%f", &input);
+    while (possible < input) {
+        if (isPrime(possible)) {
+            printf("%d\n", possible);
+        }
+        possible++;
+    }
 }
